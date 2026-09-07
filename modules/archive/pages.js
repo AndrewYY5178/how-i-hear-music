2ea400e850e4f74e6fd5cd1b3f56276f6b09ba0b
AndrewYY5178 <dyc20011223@gmail.com>
2026-09-07 18:34:13 +0800

    Tighten mobile album detail layout

--- modules/archive/pages.js
+++ modules/archive/pages.js
@@ -11,10 +11,10 @@ import { activatedTraits, tasteDNA } from "../music/taste-dna.js";
 import { metadataCoverage, metadataFields, metadataOverrideFor, metadataRows, saveMetadataOverride } from "../music/metadata.js";
 import { albumNote, saveAlbumNote } from "../music/notes.js";
 import { metadataApiRequest } from "../music/api.js";
-import { translateText } from "../layout/i18n.js?v=0.9.71";
+import { translateText } from "../layout/i18n.js?v=0.9.72";
 import { withBase } from "../layout/paths.js";
-import { archiveSearch } from "../search/pages.js?ui=3.11.55";
-import { bindCoverTones, fallbackCoverTone, reextractCoverTone } from "../layout/cover-tone.js?ui=3.11.55";
+import { archiveSearch } from "../search/pages.js?ui=3.11.56";
+import { bindCoverTones, fallbackCoverTone, reextractCoverTone } from "../layout/cover-tone.js?ui=3.11.56";
 import { coverSourcesFor } from "../music/cover-maintenance.js";
 
 const archiveNav = () => secondaryNav([["/archive/tracks", "Tracks"], ["/archive/albums", "Albums"], ["/archive/artists", "Artists"]]);
